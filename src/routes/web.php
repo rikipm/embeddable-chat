2154e6caf1f8241f6bb4c1e5fb6039065c0c5fc1
Mikhail Bratenkov <mikhailBratenkov@gmail.com>
2025-12-14 06:12:33 +0300

    demo page with iframe

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return redirect('demo.php');
+});
 
 Route::inertia('/chat', 'Chat')->name('chat');
