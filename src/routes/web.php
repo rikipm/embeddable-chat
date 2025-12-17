d75c7573c0745619d06d12ec3520ae24cbbd543a
Mikhail Bratenkov <mikhailBratenkov@gmail.com>
2025-12-17 08:54:17 +0300

    Change extension for demo page from .php to .html

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('demo.php');
+    return redirect('demo.html');
 });
 
 Route::inertia('/chat', 'Chat')->name('chat');
